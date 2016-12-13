31912bb8420f3f22e9f3093baa64bd5eb2c58745
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2016-12-13 08:16:30 +0000

    * Setting up and using govuk styling * Setting up and using partials * update gitignore to ignore npm-debug and public * Example of using middleware * Annotations of app.js

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+// mustache govuk template
+const govukTemplate = require('hof-govuk-template');
+// hogan renderer engine, this is better than hogan express because it throws an error when you've done your mustache wrong
+const hoganExpressStrict = require('hogan-express-strict');
+// Allows the use of partials
+const expressPartialTemplates = require('express-partial-templates');
+const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+// setup for front end toolkit
+govukTemplate.setup(app);
 
+// this recognises html files
+app.set('view engine', 'html');
+
+//setting where my views are, you can have arrays
+app.set('views', [path.resolve(__dirname, 'views')]);
+
+// using the hoganExpress engine
+app.engine('html', hoganExpressStrict);
+// need to call this after the others
+app.use(expressPartialTemplates(app));
+
+// serving static files
+app.use('/public', express.static(path.resolve(__dirname, 'public')));
+
+// example of passing in an object as part of middleware to all pages
+// res.locals is an object that persist for the lifetime of a request
+app.use(function(req, res, next) {
+  res.locals = Object.assign(res.locals, {
+    hello: 'Hi this is a page'
+  });
+  next();
+})
+
+// example of passing in an object as middleware just for a particular page
 app.get('/', function(req, res){
-  res.render('home');
+  res.render('template', Object.assign({}, res.locals, {
+    hello: 'SOMETHING ELSE'
+  }));
+});
+
+app.get('/test', function(req, res){
+  res.render('template');
 });
 
 // Set server port
